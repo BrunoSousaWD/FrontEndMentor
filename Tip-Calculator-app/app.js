2454305353969b7dea8437590d78168fbd3cec0b
BrunoSousaWD <43056757+BrunoSousaWD@users.noreply.github.com>
2022-11-27 14:16:03 -0800

    js start

---
+++ Tip-Calculator-app/app.js
@@ -0,0 +1,10 @@
+var val = document.getElementById('num').value;
+var tipAmount = document.getElementById('tip-result').innerText;
+var totalPerson = document.getElementById('ppl-result').innerText;
+
+
+// grab value from bill
+// grab the value from tip
+// grab the number of ppl
+// display results
+//reset button
