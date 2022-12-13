c82e67fa836e1fc1bde8b9cb6c07572c67017a00
BrunoSousaWD <43056757+BrunoSousaWD@users.noreply.github.com>
2022-12-12 21:02:07 -0800

    js

--- Tip-Calculator-app/app.js
+++ Tip-Calculator-app/app.js
@@ -1,10 +1,20 @@
-var val = document.getElementById('num').value;
-var tipAmount = document.getElementById('tip-result').innerText;
-var totalPerson = document.getElementById('ppl-result').innerText;
+var val = document.getElementById('bill-total');
+var tipAmount = document.getElementById('tip-result');
+var totalPerson = document.getElementById('ppl-result');
 
+document.querySelectorAll('input[type="number"]').forEach(input => {
+    input.oninput = () => {
+        if (input.value.length > input.maxLength)
+            input.value = input.value.slice(0, input.maxLength);
+    };
+});
 
 // grab value from bill
 // grab the value from tip
 // grab the number of ppl
 // display results
 //reset button
+
+val.addEventListener('input', (e) => {
+    tipAmount.textContent = '$' + e.target.value;
+});
