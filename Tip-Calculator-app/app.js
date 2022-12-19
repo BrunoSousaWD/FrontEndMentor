383f6effb0d665385b091176ecb957088e598b19
BrunoSousaWD <43056757+BrunoSousaWD@users.noreply.github.com>
2022-12-18 21:14:27 -0800

    js tip percent
    added code for the buttons

--- Tip-Calculator-app/app.js
+++ Tip-Calculator-app/app.js
@@ -1,6 +1,8 @@
-var val = document.getElementById('bill-total');
-var tipAmount = document.getElementById('tip-result');
-var totalPerson = document.getElementById('ppl-result');
+let val = document.getElementById('bill-total');
+let tipAmount = document.getElementById('tip-result');
+let totalPerson = document.getElementById('ppl-result');
+let tipPercent = document.querySelectorAll('.tip-p');
+
 
 document.querySelectorAll('input[type="number"]').forEach(input => {
     input.oninput = () => {
@@ -15,6 +17,12 @@ document.querySelectorAll('input[type="number"]').forEach(input => {
 // display results
 //reset button
 
-val.addEventListener('input', (e) => {
-    tipAmount.textContent = '$' + e.target.value;
-});
+for (let i = 0; i < tipPercent.length; i++) {
+    tipPercent[i].addEventListener('click', () => {
+        let y = parseInt(tipPercent[i].value) / 100;
+        let k = y * val.value;
+        console.log(k);
+        tipAmount.textContent = '$' + k;
+    })
+}
+
