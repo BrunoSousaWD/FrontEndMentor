65bc1a0e65d4626f5857cdaadd6f4e7b6929906a
BrunoSousaWD <43056757+BrunoSousaWD@users.noreply.github.com>
2022-12-18 22:07:38 -0800

    js finished
    all functions added

--- Tip-Calculator-app/app.js
+++ Tip-Calculator-app/app.js
@@ -1,9 +1,13 @@
 let val = document.getElementById('bill-total');
 let tipAmount = document.getElementById('tip-result');
-let totalPerson = document.getElementById('ppl-result');
 let tipPercent = document.querySelectorAll('.tip-p');
+let custom = document.getElementById('custom');
+let numPeople = document.getElementById('numPeople');
+let totalPerson = document.getElementById('ppl-result');
+let reset = document.getElementById('reset');
 
 
+//set max num length
 document.querySelectorAll('input[type="number"]').forEach(input => {
     input.oninput = () => {
         if (input.value.length > input.maxLength)
@@ -11,18 +15,35 @@ document.querySelectorAll('input[type="number"]').forEach(input => {
     };
 });
 
-// grab value from bill
-// grab the value from tip
-// grab the number of ppl
-// display results
-//reset button
 
+//grab bill amount * tip percent 
 for (let i = 0; i < tipPercent.length; i++) {
     tipPercent[i].addEventListener('click', () => {
         let y = parseInt(tipPercent[i].value) / 100;
         let k = y * val.value;
+        let p = k / parseInt(numPeople.value);
         console.log(k);
-        tipAmount.textContent = '$' + k;
+        tipAmount.textContent = '$' + k.toFixed(2);
+        totalPerson.textContent = '$' + p.toFixed(2);
     })
 }
 
+
+// bill amount * custom amount 
+custom.addEventListener('click', () => {
+    let c = parseInt(custom.value) / 100;
+    let u = c * val.value;
+    let p = u / parseInt(numPeople.value);
+    tipAmount.textContent = '$' + u.toFixed(2);
+    totalPerson.textContent = '$' + p.toFixed(2);
+})
+
+
+//reset
+reset.addEventListener('click', () => {
+    totalPerson.textContent = '$' + '0.00';
+    tipAmount.textContent = '$' + '0.00';
+    val.value = '';
+    numPeople.value = '';
+    custom.value = '';
+})
