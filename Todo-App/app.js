d4f92d5a7458aa7a5c84d64100f1c126e879fa01
BrunoSousaWD <43056757+BrunoSousaWD@users.noreply.github.com>
2023-02-01 18:38:39 -0800

    clearCompleted fixed

--- Todo-App/app.js
+++ Todo-App/app.js
@@ -12,7 +12,7 @@ darkMode.addEventListener('click', function () {
 function showAll() {
     let listItem = document.querySelectorAll('li');
     for (let i = 0; i < listItem.length; i++)
-        listItem[i].style.display = 'block';
+        listItem[i].style.display = 'grid';
 }
 
 //show Active
@@ -22,7 +22,7 @@ function showActive() {
         if (listItem[i].classList.contains('checked')) {
             listItem[i].style.display = 'none';
         } else {
-            listItem[i].style.display = 'block';
+            listItem[i].style.display = 'grid';
         }
 }
 
@@ -31,7 +31,7 @@ function showComplete() {
     let listItem = document.querySelectorAll('li');
     for (let i = 0; i < listItem.length; i++)
         if (listItem[i].classList.contains('checked')) {
-            listItem[i].style.display = 'block';
+            listItem[i].style.display = 'grid';
         } else {
             listItem[i].style.display = 'none';
         }
@@ -42,7 +42,8 @@ function showComplete() {
 function clearCompleted() {
     let listItem = document.querySelectorAll('.checked');
     for (let i = 0; i < listItem.length; i++)
-        listItem[i].style.display = 'none';
+        listItem[i].remove();
+
 }
 
 
@@ -93,3 +94,7 @@ function addListItem() {
 
 
 
+//missing dark mode
+//missing count
+// check style on link active
+//missing drag and drop
